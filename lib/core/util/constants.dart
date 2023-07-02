847ba9554637ad7d594a47ac08f96a9269f5c9c1
Ahmed Hallaq <a.m.hq@gmail.com>
2023-07-02 21:35:10 +0300

    create onBoarding view

--- lib/core/util/constants.dart
+++ lib/core/util/constants.dart
@@ -1,5 +1,25 @@
+import 'package:flutter/material.dart';
+
+// ============= API CONSTS =============
+
 const baseUrl = 'https://e43d-188-225-251-154.ngrok-free.app/api';
 const registerUrl = '$baseUrl/register';
 const loginUrl = '$baseUrl/login';
 const updateUserLocationUrl = '$baseUrl/update';
 const longPressShareUrl = '$baseUrl/longPressShare';
+
+// ============= STYLE CONSTS ==============
+
+const kScaffoldColor = Color(0xffFDFDFD);
+const kRedColor = Color(0xffA90606);
+
+const kPrimaryColor = Color(0xff2D2B4E);
+const kSecondaryColor = Color(0xffFFD465);
+const kOnSecondaryColor = Color(0xff784E00);
+const kDangerColor = Color(0xffF56C61);
+
+// Low Opacity Colors
+const kLightPrimaryColor = Color(0xff807D99);
+const kLightSecondaryColor = Color(0xffFFE6A6);
+const kLightDangerColor = Color(0xffFEE2E7);
+const kOnLightDangerColor = Color(0xff783341);
---
+++ lib/views/onbording/onbording_view.dart
@@ -0,0 +1,39 @@
+import 'package:betweener/core/util/assets.dart';
+import 'package:betweener/core/util/constants.dart';
+import 'package:betweener/core/widgets/secondary_button_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class OnBoardingView extends StatelessWidget {
+  const OnBoardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Spacer(),
+            SvgPicture.asset(AssetsData.onBoardingImage),
+            const Text(
+              'Just one Scan for everything',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: kPrimaryColor,
+                  fontWeight: FontWeight.w600),
+            ),
+            const Spacer(),
+            SecondaryButtonWidget(
+              text: 'Get Started',
+              width: double.infinity,
+              onTap: () {},
+            ),
+            const Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+}
