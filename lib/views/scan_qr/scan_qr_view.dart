f2e1fc7f4274726b9c1cef18c4019b3408ca7dfe
Ahmed Hallaq <a.m.hq@gmail.com>
2023-07-06 15:53:14 +0300

    create qr scan feature

--- lib/views/scan_qr/scan_qr_view.dart
+++ lib/views/scan_qr/scan_qr_view.dart
@@ -1,12 +1,63 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ScanQrView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class ScanQrView extends StatefulWidget {
   static const id = '/scanQrView';
 
   const ScanQrView({super.key});
 
   @override
+  State<ScanQrView> createState() => _ScanQrViewState();
+}
+
+class _ScanQrViewState extends State<ScanQrView> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: QRView(
+            key: qrKey,
+            overlay: QrScannerOverlayShape(),
+            onQRViewCreated: _onQRViewCreated,
+          ),
+        ),
+        Spacer()
+      ],
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
